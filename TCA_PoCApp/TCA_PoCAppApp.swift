e65b0ec5203d7bc504a8a30d3ad60ca6f47cadb5
SalvadorCarnelutti <salvadorcarnelutti@gmail.com>
2023-09-09 19:09:39 -0400

    Repo start

---
+++ TCA_PoCApp/TCA_PoCAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_PoCAppApp.swift
+//  TCA_PoCApp
+//
+//  Created by Salvador on 9/9/23.
+//
+
+import SwiftUI
+
+@main
+struct TCA_PoCAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
